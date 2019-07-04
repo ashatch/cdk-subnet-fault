a3b2e821c662262654476dbeb477612c48c27d7f
Andrew Hatch <ahatch@amplience.com>
2019-07-04 17:01:56 +0100

    initial commit - cdk 0.33.0

---
+++ bin/cdk.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import { App, Construct, Stack, StackProps } from '@aws-cdk/cdk';
+import { Vpc } from '@aws-cdk/aws-ec2';
+
+const app = new App();
+
+export interface MyStackProps extends StackProps {
+  cidr: string;
+}
+
+export class MyStack extends Stack {
+  public constructor(scope: Construct, id: string, props: MyStackProps) {
+    super(scope, id, props);
+
+    new Vpc(this, 'vpc-name', {
+      maxAZs: 12,
+      cidr: props.cidr,
+    });
+  }
+}
+
+new MyStack(app, 'mystack', {  
+  cidr: '10.101.1.0/24'
+});
---
+++ test/mystack.spec.ts
@@ -0,0 +1,23 @@
+import { expect, haveResource, countResources } from '@aws-cdk/assert';
+import { App } from '@aws-cdk/cdk';
+import { MyStack } from '../bin/cdk'
+
+
+function initializeStack(cidr: string): MyStack {
+  return new MyStack(new App(), 'Cdv2', {
+    cidr: cidr
+  });
+}
+
+test('Creates a /24 VPC with the correct subnets', (): void => {
+  const stack = initializeStack('10.101.1.0/24');
+  expect(stack).to(countResources('AWS::EC2::InternetGateway', 1));
+
+  for (let i = 0; i < 6; i++) {
+    expect(stack).to(
+      haveResource('AWS::EC2::Subnet', {
+        CidrBlock: `10.101.1.${i * 32}/27`, // 27 is the right number
+      }),
+    );
+  }
+});
